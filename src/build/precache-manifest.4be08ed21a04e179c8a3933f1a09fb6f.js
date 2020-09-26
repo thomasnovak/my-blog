6823a07acb1db5493c8c05057fa0ae13d2216440
Tom Novak <tomnovak16@gmail.com>
2020-09-26 14:39:22 -0700

    First commit

---
+++ src/build/precache-manifest.4be08ed21a04e179c8a3933f1a09fb6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49a36201cf09942b547920c70c8f4871",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55e2f0c092136fdcbc5d",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "f7742bd7ca531cfad47f",
+    "url": "/static/js/2.b09d3ca3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b09d3ca3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55e2f0c092136fdcbc5d",
+    "url": "/static/js/main.ac58eeff.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
